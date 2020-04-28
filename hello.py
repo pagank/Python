f55b541989dfa9b8044e9cc690f6bb9939488dc5
pagank <pagank@outlook.com>
2020-04-28 05:43:44 -0700

    4/28/2020

--- hello.py
+++ hello.py
@@ -1,15 +1,35 @@
-import os
-import time
-from multiprocessing import Process
 
-def do_sth(name):
-    """ proccess do """
-    print('Proccess name: {0}; pid: {1}'.format(name, os.getpid()))
-    time.sleep(112)
-    print('proccess do')
+import time
+import random
+from multiprocessing import Process,Lock
+
+
+class WriteProcess(Process):
+    """ Write file """
+
+    def __init__(self, file_name, num, lock, *args, **kwargs):
+        self.file_name = file_name
+        self.num = num
+        self.lock = lock
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        try:
+            self.lock.acquire()
+            for i in range(5):
+                with open(self.file_name, 'a+') as f:
+                    content = 'current name: {0}, pid:{1}  --- {2} \n'.format(
+                        self.name, self.pid, self.num)
+                    f.write(content)
+                    time.sleep(random.randint(1,3))
+                    print(content)
+        finally:
+            self.lock.release()
 
 
 if __name__ == "__main__":
-    p = Process(target=do_sth, args=('my proccess',))
-    p.start()
-    p.join()
+    file_name = 'test.txt'
+    lock = Lock()
+    for n in range(5):
+        p = WriteProcess(file_name, n, lock)
+        p.start()
